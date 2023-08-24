efc5744c62e9d41d56b2014f97d4038473b1ec75
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-24 23:26:19 +0300

    Completed lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -5,27 +5,41 @@ class CashRegister:
         self.discount = discount
         self.total = 0
         self.items = []
+        self.prices = []
 
     def add_item(self, title, price, quantity=1):
         self.total += price * quantity
+        self.prices.append(price)
         self.items.extend([title] * quantity)
 
     def apply_discount(self):
         if self.discount > 0:
-            discount_amount = self.total * (self.discount / 100)
-            self.total -= discount_amount
-            self.total = round(self.total, 2)
-            return f"After the discount, the total comes to ${self.total:.2f}."
-        else:
-            return "There is no discount to apply."
+            discount_amount = self.total * self.discount / 100
+            rounded_discount = round(discount_amount)
+            self.total -= rounded_discount
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:    
+            print("There is no discount to apply.")
+
+    def get_all_items(self):
+        return self.items
 
     def void_last_transaction(self):
         if self.items:
-            last_item_price = self.get_price_for_item(self.items[-1])
+            last_item_price = self._get_last_item_price()
             self.total -= last_item_price
             self.items.pop()
+        else:
+            print("No items to void.")
+            self.total = 0.0  # Reset the total to 0 if no items are left
+
+    def _get_last_item_price(self):
+        last_item = self.items[-1]
+        # Use the item_prices dictionary to look up the price of the last_item.
+        return self.get_price_for_item(last_item)
 
     def get_price_for_item(self, item_title):
+        # For demonstration purposes, we'll use a dictionary to store item prices.
         item_prices = {
             "eggs": 0.98,
             "book": 5.00,
@@ -33,5 +47,8 @@ class CashRegister:
             "Ritz Crackers": 5.0,
             # ... add more items and prices as needed ...
         }
+
+        # Use the dictionary's get() method to retrieve the price for the given item_title.
+        # Provide a default value of 0.0 if the item_title is not found in the dictionary.
         return item_prices.get(item_title, 0.0)
     
